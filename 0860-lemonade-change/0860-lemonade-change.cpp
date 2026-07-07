a0a232ea31bff47fdab20d19408e360edf50b9f9
Amritesh <kvamriteshkumar@gmail.com>
2026-07-07 12:44:23 +0530

    Time: 3 ms (10.26%), Space: 87 MB (96.81%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+//When multiple greedy choices work, preserve the resource that is harder to replace in the future.
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
+                five++;
+            }else if(bills[i]==10){
+                if(five>0){
+                    five--;
+                    ten++;
+                }else{
+                    return false;
+                }
+            }else{
+                if(five>0 && ten>0){
+                    five--;
+                    ten--;
+                }else if(five>=3){
+                    five -=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
