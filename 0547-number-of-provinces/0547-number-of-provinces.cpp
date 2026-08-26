50760c1694d6cc55d4b7f87826167ee1d5d20ceb
Amritesh <kvamriteshkumar@gmail.com>
2026-08-26 11:58:35 +0530

    Time: 5 ms (15.65%), Space: 21.4 MB (6.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-
-// space= O(N) for visited array + O(N) for recursion stack space....
-// time= O(N) + O(V+2E) first for outer loop and the total dfs calls !
-    void solve(int node ,vector<vector<int>>& adj,vector<int> &vis ){
-        vis[node]=1;
-        // dfs.push_back(node);
-        for(auto it:adj[node]){
-            if(!vis[it]) solve(it,adj,vis);
-        }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
+        // bfs approch !
+        // firstly we have to amke an adjency list!!!
+
+        int n = isConnected.size();
 
         vector<vector<int>> adj(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && i!=j){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
 
-        vector<int> vis(n+1 , 0);
-        // vector<int>dfs;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                ans++;
-                solve(i , adj , vis);
+        int count = 0;
+        vector<int> vis(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+
+            if (!vis[i]) {
+                vis[i] = 1;
+                count++;
+                queue<int> q;
+                q.push(i);
+
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+                    for (auto it : adj[node]) {
+                        if (!vis[it]) {
+                            vis[it] = 1;
+                            q.push(it);
+                        }
+                    }
+                }
             }
         }
-        return ans;
+
+        return count;
     }
 };
